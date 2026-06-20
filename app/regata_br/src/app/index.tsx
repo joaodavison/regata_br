8d2545314878eb4d598d6f6b3b120ecee7ccddd5
Joao Davison <joao.davison@embraer.com.br>
2026-06-20 20:34:40 -0300

    Estilo e botão

--- app/regata_br/src/app/index.tsx
+++ app/regata_br/src/app/index.tsx
@@ -1,14 +1,30 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, Button, Alert } from 'react-native';
 
 export default function Index() {
+
+    function funcContagem(){
+      const tempo = 10
+      return Alert.alert(`T = ${tempo}`)
+    }
+
   return (
-    <View>
-      <Text></Text>
-      <Text></Text>
-      <Text></Text>
-      <Text>  Bem vindo ao meu aplicativo novo</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Regata BR</Text>
+      <Button title="Contagem" onPress={funcContagem} />
     </View>
   );
 }
 
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 32,
+    justifyContent: "center",
+  },
+  title:{
+    color: "#334462",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+})
