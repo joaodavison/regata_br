aa80f191a2931587e2d85651b86b0a217ebdebdd
Joao Davison <joao.davison@embraer.com.br>
2026-06-20 17:47:31 -0300

    Fisrt app

---
+++ app/regata_br/src/app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+      <Text>  Bem vindo ao meu aplicativo novo</Text>
+    </View>
+  );
+}
+
+
