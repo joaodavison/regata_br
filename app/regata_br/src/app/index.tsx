ac242fc58aa7cfb3611538b40b9fd6de872d0ff1
Joao Davison <joao.davison@embraer.com.br>
2026-06-23 18:46:09 -0300

    Adding GPS to main page

--- app/regata_br/src/app/index.tsx
+++ app/regata_br/src/app/index.tsx
@@ -3,6 +3,7 @@ import { Text, View, StyleSheet, Alert } from 'react-native';
 import { Button} from "../components/button"
 import { Input} from "../components/input"
 import { router } from "expo-router"
+import * as Location from 'expo-location';
 import Page2 from "./gps";
 
 export default function Index() {
@@ -10,20 +11,52 @@ export default function Index() {
   // estado atualizavel na renderizacao
   const [name, setName] = useState("teste")
   const [counter, setCounter] = useState(-300) // 5 min
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
 
   // var global (nao atualiza na renderizacao)
   // let name = ""
 
   // chamada temporizada (1s) 
   useEffect(() => {
-    const intervalo = setInterval(() => {
+    const interval = setInterval(() => {
       setCounter((counter) => counter + 1);
     }, 1000);
 
     // limpa o intervalo quando o componente desmonta ou muda
-    return () => clearInterval(intervalo);
+    return () => clearInterval(interval);
   }, [counter]);
 
+  // chamada temporizada (5s) 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      getLocation();
+    }, 5000);
+
+    // limpa o intervalo quando o componente desmonta ou muda
+    return () => clearInterval(interval);
+  }, []);  
+
+  // gps
+  async function getLocation() {
+
+    // solicita a permissão de acesso ao GPS
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      return;
+    }
+
+    // captura as coordenadas
+    try {
+      let currentLocation = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.High,
+      });
+      setLocation(currentLocation);
+    }
+    catch (error) {
+    } 
+    // finally {
+    // }
+  }  
 
   // funcs auxiliares
   function funcContagem(){
@@ -40,11 +73,27 @@ export default function Index() {
     router.navigate("./gps")
   }
 
+  function calcTime(timestamp: number){
+    return Math.trunc(timestamp / 1000 - 1782200000)
+  }
+
+  function calcLat(latitude: number){
+    return Math.trunc(-10000 * (23.22 + latitude))
+  } 
+
+  function calcLong(longitude: number){
+    return Math.trunc(-10000 * (45.90 + longitude))
+  }  
+
   // componentes renderizados no app
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Regata BR - var: {name}</Text>
       <Text style={styles.title}>Contagem: {counter}</Text>
+
+      { /* mostra location se tiver dados */}
+      {
+      location && (<Text style={styles.title}>{calcLat(location.coords.latitude)}, {calcLong(location.coords.longitude)}  @{calcTime(location.timestamp)}</Text>)}
 
       { /* input com onChange + arrow function */}
       <Input onChangeText={(text) => funcText(text)}></Input>
